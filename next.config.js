271e7f71b1244048fe32946dd9fb4a34e12f3994
Amalendu Pandey <amalendupandey56@gmail.com>
2022-05-11 09:45:19 +0530

    Header component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['images.ctfassets.net','s3.coinmarketcap.com','s2.coinmarketcap.com','encrypted-tbn0.gstatic.com'],
+  },
 }
